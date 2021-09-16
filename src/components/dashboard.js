87eea864a13c53b8ae8a8550979713b45c5915b4
nitingangyan <nitingangyan@gmail.com>
2021-09-16 13:31:11 +0530

    2 files modified

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -13,10 +14,17 @@ const useStyles = makeStyles(theme => ({
 
 const Dashboard = () => {
   const classes = useStyles();
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/dashboard/').then(response => {
+      console.log(response);
+      setData(response);
+    });
+  }, []);
   return (
     <div style={{ textAlign: 'center', width: '100%' }}>
       <Card className={classes.root}>
-        <CardContent />
+        <CardContent>Test</CardContent>
         <CardActions />
       </Card>
     </div>
